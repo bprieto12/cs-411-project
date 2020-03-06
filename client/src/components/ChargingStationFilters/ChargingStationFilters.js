52b460b523b354f499d68fa4e413c4fef449f226
Brenden Prieto <12bprieto@gmail.com>
2020-03-05 21:49:35 -0800

    removed the Aux code for windows users lol

--- client/src/components/ChargingStationFilters/ChargingStationFilters.js
+++ client/src/components/ChargingStationFilters/ChargingStationFilters.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Aux from '../../hoc/Aux';
 import styles from './ChargingStationFilters.module.css';
 
 const ChargingStationFilters = props => {
     return (
-        <Aux>
+        <Fragment>
             <button className={styles.FavoritesBtn}>My Favorites</button>
             <button className={styles.FavoritesBtn}>Sort by</button>
-        </Aux>
+        </Fragment>
     );
 }
 
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -60,7 +60,6 @@ class App extends Component {
 						<Route path="/">
 							<Home />
 						</Route>
-						
 					</Switch>
 		      	</Layout>
 		    </div>
